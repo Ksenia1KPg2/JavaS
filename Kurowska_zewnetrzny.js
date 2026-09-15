5ef9c67894d7b371db04ab0eee24bf081a3f38d2
Ksenia1KPg2 <kseniakurowska10@gmail.com>
2026-09-16 00:44:52 +0200

    Add files via upload

---
+++ Kurowska_zewnetrzny.js
@@ -0,0 +1,3 @@
+document.write("Mój numer w dzienniku to 8"+"<br>"+"pierwszy wiersz to: 9 Nie Tak 6"+"<br>"+"Drugi wiersz to: 9 Nie Tak 6"+"<br>");
+document.write("ostatnia modyfikacja strony".strike().fontcolor("green").fontsize(6)+"<br>"); 
+document.write(document.lastModified.strike().fontcolor("green").fontsize(6)+"<br>"); 
